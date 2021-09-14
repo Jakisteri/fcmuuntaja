aef4253069950a0d5165d5c12c51adac765c3585
Jarkko <jarkko.maki@seamk.fi>
2021-09-14 20:17:17 +0300

    javascriptiin lisätty tavaraa, puuttuu viä kaava

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-function muunnaFC() {
-    
+function fahrenheittocelsius(fahrenheit) {
+    let celsius = fahrenheit;
+    return celsius;
 }
 
-const button = document.querySelector("button");
-button.addEventListener("click", muunnaFC);
+
+function muunnaFC() {
+    let syötetty = syötettyfahrenheit.value;
+    tulostefahrenheit.textContent = syötetty;
+
+    tulostecelsius.textContent = fahrenheittocelsius(syötetty);
+}
+
+const syötettyfahrenheit = document.querySelector("input");
+
+const tulostefahrenheit = document.querySelector("#fahrenheit");
+
+const tulostecelsius = document.querySelector("#celsius");
+
+const muuntonappi = document.querySelector("button");
+muuntonappi.addEventListener("click", muunnaFC);
