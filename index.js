adb20993271f5356bf3376461fbe63fdaa623ec8
Jarkko <jarkko.maki@seamk.fi>
2021-09-25 14:44:23 +0300

    Added C to F converter bellow F to C converter

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
+//Fahrenheit to Celsius conversion
 function fahrenheittocelsius(fahrenheit) {
     let celsius = ((fahrenheit - 32) / 1.8).toFixed(1);
     return celsius;
 }
 
+function convertFC() {
+    let usersFahrenheit = inputFahrenheit.value;
+    printFahrenheit.textContent = usersFahrenheit;
 
-function muunnaFC() {
-    let syötetty = syötettyfahrenheit.value;
-    tulostefahrenheit.textContent = syötetty;
-
-    tulostecelsius.textContent = fahrenheittocelsius(syötetty);
+    printCelsius.textContent = fahrenheittocelsius(usersFahrenheit);
 }
 
-const syötettyfahrenheit = document.querySelector("input");
+const inputFahrenheit = document.getElementById("inputFC");
 
-const tulostefahrenheit = document.querySelector("#fahrenheit");
+const printFahrenheit = document.getElementById("fahrenheit1");
 
-const tulostecelsius = document.querySelector("#celsius");
+const printCelsius = document.getElementById("celsius1");
 
-const muuntonappi = document.querySelector("button");
-muuntonappi.addEventListener("click", muunnaFC);
+const convertbutton = document.getElementById("buttonFC");
+convertbutton.addEventListener("click", convertFC);
+
+//Celsius to Fahrenheit conversion
+function celsiustofahrenheit(celsius) {
+    let fahrenheit = ((celsius * 1.8) + 32).toFixed(1);
+    return fahrenheit;
+}
+
+function convertCF() {
+    let usersCelsius = inputCelsius.value;
+    printCelsius2.textContent = usersCelsius;
+
+    printFahrenheit2.textContent = celsiustofahrenheit(usersCelsius);
+}
+
+const inputCelsius = document.getElementById("inputCF");
+
+const printCelsius2 = document.getElementById("celsius2");
+
+const printFahrenheit2 = document.getElementById("fahrenheit2");
+
+const convertbutton2 = document.getElementById("buttonCF");
+convertbutton2.addEventListener("click", convertCF);
